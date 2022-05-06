60940810e6ff220b6c65ac2c0ba36fb8ed22dc0f
Kamil_Niemiec <kamil_niemiec@o2.pl>
2022-05-06 14:30:21 +0200

    Updates

---
+++ precache-manifest.fef06dc703650bdca1a6202a1f6eaa20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109e9e6dad862dc6b98e0e44f6e648a3",
+    "url": "/memoapp/index.html"
+  },
+  {
+    "revision": "887c79fd0d91cb89ae38",
+    "url": "/memoapp/static/css/main.7dbcbc04.chunk.css"
+  },
+  {
+    "revision": "6e1fcc48ff78ee4469ff",
+    "url": "/memoapp/static/js/2.9d93751d.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/memoapp/static/js/2.9d93751d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887c79fd0d91cb89ae38",
+    "url": "/memoapp/static/js/main.2c0b78f4.chunk.js"
+  },
+  {
+    "revision": "a92018247f0a7b460dcd",
+    "url": "/memoapp/static/js/runtime-main.e31aa541.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/memoapp/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/memoapp/static/media/memoriesText.2b89c5fa.png"
+  }
+]);

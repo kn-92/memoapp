a02e29d6b5a7418feae184c4c1d8b84d09f92614
Kamil_Niemiec <kamil_niemiec@o2.pl>
2022-05-06 14:06:38 +0200

    final

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,14 @@
+export const FETCH_ALL = "FETCH_ALL";
+export const FETCH_POST = "FETCH_POST";
+export const FETCH_BY_SEARCH = "FETCH_BY_SEARCH";
+export const CREATE = "CREATE";
+export const UPDATE = "UPADTE";
+export const DELETE = "DELETE";
+export const UPDATE_LIKE = "UPDATE_LIKE";
+
+export const AUTH = "AUTH";
+export const LOGOUT = "LOGOUT";
+
+export const END_LOADING = "END_LOADING";
+export const START_LOADING = "START_LOADING";
+export const COMMENT = "COMMENT";
---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+
+    let decodedData;
+
+    if (token.length < 1000) {
+      decodedData = jwt.verify(token, "test");
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+
+      req.userId = decodedData?.sub;
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
